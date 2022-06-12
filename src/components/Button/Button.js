5bafa890135c4c61482380d0d4c1a4ca42603dc1
jieun0411 <annakim0411@naver.com>
2022-06-12 22:46:55 +0900

    [#4] feat: Button 컴포넌트 구현

---
+++ src/components/Button/Button.js
@@ -0,0 +1,93 @@
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import Common from "../../styles/common";
+
+const StyledButton = styled.button`
+  ${({
+    width,
+    height,
+    backgroundColor,
+    color,
+    fontSize,
+    borderWidth,
+    borderColor,
+    cursor,
+    style,
+    transitionDuration,
+    hover,
+    active,
+  }) => ({
+    width,
+    height,
+    backgroundColor,
+    color,
+    fontSize,
+    borderWidth,
+    borderColor,
+    cursor,
+    transitionDuration,
+    "&:hover": hover,
+    "&:active": active,
+    ...style,
+  })}
+  border: ${(borderWidth, borderColor) =>
+    borderWidth ? `${borderWidth} solid ${borderColor}` : "none"};
+  border-radius: ${borderRadius => borderRadius || "15px"};
+`;
+
+const Button = ({
+  width = "100%",
+  height = "70px",
+  backgroundColor = `${Common.colors.mainColor}`,
+  color = "#dddfeb",
+  fontSize = `${Common.fontSize.fs16}`,
+  borderWidth = 0,
+  borderRadius = "15px",
+  borderColor = "none",
+  cursor = "pointer",
+  transitionDuration = "0.2s",
+  hover = { backgroundColor: "#4f508f" },
+  active = { backgroundColor: "#434379" },
+  children,
+  onClick,
+  ...props
+}) => (
+  <StyledButton
+    type="button"
+    width={width}
+    height={height}
+    backgroundColor={backgroundColor}
+    color={color}
+    fontSize={fontSize}
+    borderWidth={borderWidth}
+    borderRadius={borderRadius}
+    borderColor={borderColor}
+    cursor={cursor}
+    transitionDuration={transitionDuration}
+    hover={hover}
+    active={active}
+    onClick={onClick}
+    {...props}
+  >
+    {children}
+  </StyledButton>
+);
+
+Button.propTypes = {
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
+  fontSize: PropTypes.string,
+  borderWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  borderRadius: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  borderColor: PropTypes.string,
+  cursor: PropTypes.string,
+  transitionDuration: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  hover: PropTypes.shape,
+  active: PropTypes.shape,
+  onClick: PropTypes.func,
+  children: PropTypes.string,
+};
+
+export default Button;
---
+++ src/stories/components/Button.stories.js
@@ -0,0 +1,43 @@
+import Button from "../../components/Button/Button";
+import Common from "../../styles/common";
+
+export default {
+  title: "Component/Button",
+  component: Button,
+  argTypes: {
+    width: {
+      control: {
+        type: "range",
+        min: 100,
+        max: 500,
+        step: 10,
+      },
+    },
+    height: {
+      control: {
+        type: "range",
+        min: 10,
+        max: 100,
+        step: 10,
+      },
+    },
+    color: {
+      options: ["#dddfeb", "black", "white"],
+      control: "select",
+    },
+    backgroundColor: {
+      options: [`${Common.colors.mainColor}`, "white"],
+      control: "select",
+    },
+    onClick: { action: "onClick" },
+    borderColor: {
+      options: ["black", "white"],
+      control: "select",
+    },
+    borderRadius: { control: "text" },
+    borderWidth: { control: "text" },
+    children: { control: "text" },
+  },
+};
+
+export const Default = args => <Button {...args}>버튼</Button>;
