2ec00a6df4f3033f3983bfcd9ad756701a5425ae
violet9503 <violet9503@gmail.com>
2022-06-16 17:32:13 +0900

    [#45] feat: 전역 관리 store 추가 및 story 예시 코드 작성

---
+++ src/stories/store/GlobalContext.stories.js
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../store/GlobalProvider";
+
+export default {
+  title: "Component/GlobalContext",
+  argTypes: {},
+};
+
+export const Default = () => {
+  const { state, dispatch, deleteTodo } = useContext(GlobalContext);
+
+  const addTodo = text =>
+    dispatch({
+      type: "ADD_TODO",
+      text,
+    });
+
+  return (
+    <>
+      <button type="button" onClick={() => addTodo("Hello")}>
+        Add
+      </button>
+      <button type="button" onClick={() => deleteTodo(2)}>
+        delete
+      </button>
+      <ul>
+        {state.todos.map(todo => (
+          <li>{todo.todo}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
