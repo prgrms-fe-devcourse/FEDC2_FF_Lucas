ac71e8b4bc6d27c734bd010e07cac15c35248155
violet9503 <violet9503@gmail.com>
2022-06-17 00:12:34 +0900

    [#53] feat: api 예시 코드 추가, storybook api 세팅

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,25 @@
 import { Global } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import axios from "axios";
 import resetStyle from "../src/styles/reset";
 import globalStyle from "../src/styles/style";
 import GlobalProvider from "../src/store/GlobalProvider";
 
+const queryClient = new QueryClient();
+
+axios.defaults.baseURL = `http://kdt.frontend.2nd.programmers.co.kr:5006`;
+
 export const decorators = [
   Story => (
     <>
-      <Global styles={[resetStyle, globalStyle]} />
-      <GlobalProvider>
-        <Story />
-      </GlobalProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Global styles={[resetStyle, globalStyle]} />
+        <GlobalProvider>
+          <Story />
+        </GlobalProvider>
+      </QueryClientProvider>
     </>
   ),
 ];
