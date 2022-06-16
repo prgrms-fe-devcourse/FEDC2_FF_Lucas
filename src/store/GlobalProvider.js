c7902099d61868846cd6cd532afea9cf9196501c
violet9503 <violet9503@gmail.com>
2022-06-16 17:32:13 +0900

    [#45] feat: 전역 관리 store 추가 및 story 예시 코드 작성

---
+++ src/store/GlobalProvider.js
@@ -0,0 +1,57 @@
+import { useReducer, createContext, useMemo } from "react";
+import { PropTypes } from "prop-types";
+
+const initialState = {
+  todos: [
+    { id: 1, todo: "create context" },
+    { id: 2, todo: "provide context" },
+  ],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO": {
+      return {
+        ...state,
+        todos: state.todos.concat({ id: 4, todo: action.text }),
+      };
+    }
+    case "DELETE_TODO": {
+      return {
+        ...state,
+        todos: state.todos.filter(todo => action.id !== todo.id),
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export const GlobalContext = createContext(null);
+
+const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const deleteTodo = id =>
+    dispatch({
+      type: "DELETE_TODO",
+      id,
+    });
+
+  const provideValue = useMemo(
+    () => ({ state, dispatch, deleteTodo }),
+    [state, dispatch],
+  );
+
+  return (
+    <GlobalContext.Provider value={provideValue}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+GlobalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default GlobalProvider;
