6b4adcf40131190678ed44b21686abd39d359484
jieun0411 <annakim0411@naver.com>
2022-06-17 02:23:17 +0900

    [#46] feat: Profile 페이지 컴포넌트 구현

---
+++ src/stories/pages/Profile/Profile.stories.js
@@ -0,0 +1,8 @@
+import Profile from "../../../pages/Profile/Profile";
+
+export default {
+  title: "Page/Profile",
+  component: Profile,
+  argTypes: {},
+};
+export const Default = args => <Profile {...args} />;
