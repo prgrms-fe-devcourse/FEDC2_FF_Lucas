f505ea2ac50bdd93a093cf46f18aafb965cc60ce
violet9503 <violet9503@gmail.com>
2022-06-17 16:40:07 +0900

    [#59] feat: react-router 추가 및 메인, 로그인, 로그아웃 라우트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Global } from "@emotion/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import resetStyle from "./styles/reset";
 import globalStyle from "./styles/style";
+import MainPage from "./pages/MainPage/MainPage";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 axios.defaults.baseURL = `http://kdt.frontend.2nd.programmers.co.kr:5006`;
 
@@ -9,7 +13,14 @@ function App() {
   return (
     <div className="App">
       <Global styles={[resetStyle, globalStyle]} />
-      FF
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          {/* <Route path="*" element={<NotFound />}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
