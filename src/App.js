20eb94c03b542c5fa8702f2dbafc49ef8ea2be93
Hoseok Na <hsna7024@gmail.com>
2022-06-18 11:07:05 +0900

    [#73] feat: App에서 채널정보 전역상태에서 저장

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { Global } from "@emotion/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
+import { useEffect } from "react";
 import resetStyle from "./styles/reset";
 import globalStyle from "./styles/style";
 import MainPage from "./pages/MainPage/MainPage";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import { useGetChannelList } from "./utils/apis/channels";
+import { useGlobalContext } from "./store/GlobalProvider";
 
 axios.defaults.baseURL = `http://kdt.frontend.2nd.programmers.co.kr:5006`;
 
 function App() {
+  const { setChannels } = useGlobalContext();
+  const { data: channels } = useGetChannelList();
+
+  useEffect(() => {
+    setChannels(channels);
+  }, [channels]);
+
   return (
     <div className="App">
       <Global styles={[resetStyle, globalStyle]} />
