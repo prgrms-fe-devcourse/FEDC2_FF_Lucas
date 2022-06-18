333b8d83ac50a0288137aa1ce0b3183acb37a50e
jieun0411 <annakim0411@naver.com>
2022-06-19 01:01:22 +0900

    [#72] feat: 프로필 수정 페이지 컴포넌트 구현

---
+++ src/stories/pages/UpdateProfile/UpdateProfile.stories.js
@@ -0,0 +1,8 @@
+import UpdateProfile from "../../../pages/UpdateProfile/UpdateProfile";
+
+export default {
+  title: "Page/UpdateProfile",
+  component: UpdateProfile,
+  argTypes: {},
+};
+export const Default = args => <UpdateProfile {...args} />;
