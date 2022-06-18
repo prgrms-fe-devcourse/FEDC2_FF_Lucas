254b3dfdc82a88a5f53f776e21318e0ad82e5b89
Hoseok Na <hsna7024@gmail.com>
2022-06-18 19:25:29 +0900

    [#27] feat: post 등록 api 연동

--- src/stories/pages/PostPage/WritingPostPage.stories.js
+++ src/stories/pages/PostPage/WritingPostPage.stories.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import WritingPostPage from "../../../pages/Post/WritingPostPage";
+import { useGetChannelList } from "../../../utils/apis/channels";
+import { useGlobalContext } from "../../../store/GlobalProvider";
 
 export default {
   title: "Page/WitePostingPage",
   component: WritingPostPage,
 };
 
-export const Default = args => <WritingPostPage {...args} />;
+export const Default = args => {
+  const { setChannels } = useGlobalContext();
+  const { data: channels } = useGetChannelList();
+
+  useEffect(() => {
+    setChannels(channels);
+  }, [channels]);
+
+  return <WritingPostPage {...args} />;
+};
