60b012dc3e007e0c5fc38a3a0c427d4f487352e0
Hoseok Na <hsna7024@gmail.com>
2022-06-19 11:51:19 +0900

    [#65] feat: 404페이지  스토리북 구현

---
+++ src/stories/pages/NotFound/NotFoundPage.stories.js
@@ -0,0 +1,8 @@
+import NotFoundPage from "../../../pages/NotFound/NotFoundPage";
+
+export default {
+  title: "Page/NotFound",
+  component: NotFoundPage,
+};
+
+export const Default = args => <NotFoundPage {...args} />;
