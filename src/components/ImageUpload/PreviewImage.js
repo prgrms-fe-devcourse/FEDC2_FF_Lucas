c249c4a68afaa309fe40eb2c28d3fa5b3049322a
Hoseok Na <hsna7024@gmail.com>
2022-06-20 23:02:37 +0900

    [#106] fix: Wrapper 위치 변경해서 리렌더링 방지

--- src/components/ImageUpload/PreviewImage.js
+++ src/components/ImageUpload/PreviewImage.js
@@ -2,28 +2,26 @@ import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 import React from "react";
 
-const PreviewImage = ({ children, previewImageWrapperStyles, previewItem }) => {
-  const PreviewImageWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 40px;
-    padding-left: 40px;
-    background-color: #d9d9d9;
-    width: 100%;
-    height: 245px;
-    border-radius: 15px;
-    box-sizing: border-box;
-  `;
-  console.log(previewImageWrapperStyles);
+const PreviewImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+  padding-left: 40px;
+  background-color: #d9d9d9;
+  width: 100%;
+  height: 245px;
+  border-radius: 15px;
+  box-sizing: border-box;
+`;
 
-  return children ? (
+const PreviewImage = ({ children, previewImageWrapperStyles, previewItem }) =>
+  children ? (
     React.cloneElement(children, null, [previewItem])
   ) : (
     <PreviewImageWrapper style={previewImageWrapperStyles}>
       {previewItem}
     </PreviewImageWrapper>
   );
-};
 
 PreviewImage.propTypes = {
   children: PropTypes.node,
