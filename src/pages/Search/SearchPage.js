ae47fdd9b0e545e4cabee2bb9fa0a34727127a93
violet9503 <violet9503@gmail.com>
2022-06-22 00:51:15 +0900

    [#140] feat: 프로필, 검색결과 상세 페이지 렌더링 적용

--- src/pages/Search/SearchPage.js
+++ src/pages/Search/SearchPage.js
@@ -1,11 +1,15 @@
 import styled from "@emotion/styled";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import UpperHeader from "../../components/Header/UpperHeader";
 import Card from "../../components/Card/Card";
 import UserSearch from "../../components/UserSearch/UserSearch";
 import Text from "../../components/Text/Text";
 import Footer from "../../components/Footer/Footer";
+import Modal from "../../components/Modal/Modal";
+import DetailPage from "../../components/DetailPage/DetailPage";
 import useSearch from "../../utils/apis/search";
+import { useGetPostByPostId } from "../../utils/apis/posts";
 
 const Wrapper = styled.div`
   margin: 50px 15%;
@@ -33,6 +37,40 @@ export default function SearchPage() {
   const location = useLocation();
   query = location.state.resultlist;
   const { data = [] } = useSearch(query);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPostId, setSelectedPostId] = useState(null);
+  const [selectedPost, setSeletedPost] = useState(null);
+  const [postArr, setPostArr] = useState([]);
+  const { data: postData } = useGetPostByPostId({ postId: selectedPostId });
+
+  const onHandlePost = ({ changedTarget, postId }) => {
+    const tempPost = postArr.map(post =>
+      post._id === postId ? { ...post, ...changedTarget } : post,
+    );
+    setSeletedPost({ ...selectedPost, ...changedTarget });
+    setPostArr(tempPost);
+  };
+
+  const onDeletePost = postId => {
+    setSeletedPost(null);
+    setIsModalOpen(false);
+    setPostArr(postArr.filter(post => post._id !== postId));
+  };
+
+  const onClickPost = postId => {
+    setSelectedPostId(postId);
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    if (!postData) return;
+    setSeletedPost(postData);
+  }, [postData]);
+
+  useEffect(() => {
+    if (!data) return;
+    setPostArr(data);
+  }, [data]);
 
   return (
     <>
@@ -45,7 +83,7 @@ export default function SearchPage() {
           사용자
         </StyledText>
         <UserWrapper>
-          {data.map(e =>
+          {postArr.map(e =>
             e !== undefined && e.role ? (
               <UserSearch userName={e.fullName} key={e._id} />
             ) : null,
@@ -57,10 +95,10 @@ export default function SearchPage() {
           스타일
         </StyledText>
         <PostWrapper>
-          {data.map(e =>
+          {postArr.map(e =>
             e !== undefined && e.role === undefined ? (
               <Card
-                width="300"
+                width={300}
                 commentCount={e.comments.length}
                 date={e.createdAt.slice(0, 10)}
                 likeCount={e.likes.length}
@@ -69,11 +107,27 @@ export default function SearchPage() {
                 key={e._id}
                 profileImg
                 userName
+                onClick={() => onClickPost(e._id)}
               />
             ) : null,
           )}
         </PostWrapper>
       </Wrapper>
+      <Modal
+        width="80%"
+        visible={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        {selectedPost ? (
+          <DetailPage
+            post={selectedPost}
+            onHandlePost={onHandlePost}
+            onDeletePost={onDeletePost}
+          />
+        ) : (
+          <>No Post</>
+        )}
+      </Modal>
       <Footer />
     </>
   );
