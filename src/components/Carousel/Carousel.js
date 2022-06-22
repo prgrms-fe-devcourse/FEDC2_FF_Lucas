f89b62fb868533a3542587e0fd468c5da84d12e5
yoo chang heon <dbckdgjs1130@naver.com>
2022-06-22 16:40:18 +0900

    [#164] feat: 캐러셀 이미지 추가

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,6 +3,11 @@ import styled from "@emotion/styled";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import banner1 from "../../images/banner1.jpg";
+import banner2 from "../../images/banner2.jpg";
+import banner3 from "../../images/banner3.jpg";
+import banner4 from "../../images/banner4.jpg";
+import main2 from "../../images/main2.png";
 
 const StyledSlider = styled(Slider)`
   .slick-slide div {
@@ -11,7 +16,7 @@ const StyledSlider = styled(Slider)`
   .slick-track div {
     display: flex;
     justify-content: center;
-    background-color: #d9d9d9;
+    // background-color: #d9d9d9;
   }
 `;
 
@@ -31,26 +36,27 @@ export default function Carousel({ height, second }) {
     autoplaySpeed: second,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
+    centerMode: true,
+    slidesToShow: 1,
   };
   return (
     <StyledSlider {...settings}>
       <ImageContainer>
-        <Image height={height} src="https://picsum.photos/200" />
+        <Image height={height} src={main2} />
       </ImageContainer>
       <ImageContainer>
-        <Image height={height} src="https://picsum.photos/200" />
+        <Image height={height} src={banner1} />
       </ImageContainer>
       <ImageContainer>
-        <Image height={height} src="https://picsum.photos/200" />
+        <Image height={height} src={banner2} />
       </ImageContainer>
       <ImageContainer>
-        <Image height={height} src="https://picsum.photos/200" />
+        <Image height={height} src={banner3} />
       </ImageContainer>
       <ImageContainer>
-        <Image height={height} src="https://picsum.photos/200" />
+        <Image height={height} src={banner4} />
       </ImageContainer>
     </StyledSlider>
   );
